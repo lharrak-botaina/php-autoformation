cf20b88cfcb2558086267923855856a00ac7c89c
lharrak-botaina <lharrak.botaina@gmail.com>
2022-01-25 15:30:37 +0100

    add array

---
+++ syntax/array.php
@@ -0,0 +1,5 @@
+
+<?php
+$cars = array("Volvo", "BMW", "Toyota"); 
+echo "cars name : " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
---
+++ syntax/constants.php
@@ -0,0 +1,14 @@
+<?php
+// Specifies the name of the constant
+// value: Specifies the value of the constant
+define("GREETING", "hello hello !!");
+echo GREETING;
+
+
+define("cars", [
+    "Alfa Romeo",
+    "BMW",
+    "Toyota"
+  ]);
+  echo cars[2];
+?> 
