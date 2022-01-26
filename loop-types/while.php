833df0b8c67615e75bab8c052386cba1ed36134c
lharrak-botaina <lharrak.botaina@gmail.com>
2022-01-26 12:48:18 +0100

    foreach

---
+++ loop-types/while.php
@@ -0,0 +1,16 @@
+<?php  
+// $x = 1;
+ 
+// while($x <= 5) {
+//   echo "The number is: $x <br>";
+//   $x++;
+// } 
+
+
+$x = 0;
+ 
+while($x <= 100) {
+  echo "The number is: $x <br>";
+  $x+=10;
+}
+?>  
