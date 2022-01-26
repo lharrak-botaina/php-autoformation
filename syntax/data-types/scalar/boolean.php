26916456a6f9d6dcf2b75a91c7e5936beceede11
lharrak-botaina <lharrak.botaina@gmail.com>
2022-01-26 10:52:33 +0100

    add integer

---
+++ syntax/data-types/scalar/boolean.php
@@ -0,0 +1,11 @@
+<?php
+
+$is_admin = true;
+$is_user_logged_in = false;
+
+echo var_dump($is_admin) ;
+echo var_dump( $is_user_logged_in);
+
+//1 is true
+//0 is false
+?>  
---
+++ syntax/data-types/scalar/intg.php
@@ -0,0 +1,7 @@
+
+<?php  
+$x = 5985;
+$y=pi();
+var_dump($x);
+var_dump($y);
+?>  
