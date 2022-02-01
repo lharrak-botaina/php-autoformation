3f156a789d39af684e1bc53ab681915ff654a099
lharrak-botaina <lharrak.botaina@gmail.com>
2022-02-01 10:46:38 +0100

    update

---
+++ get-post/get/form.php
@@ -0,0 +1,8 @@
+<?php
+
+
+if($_GET['email'] ){
+        
+    echo "Welcome " . $_GET['email'] . "<br />";
+    
+}
--- get-post/get/get.php
+++
@@ -1,2 +0,0 @@
-Bienvenue <?php echo $_GET["fnom"] ; ?> ! <br />
-	Votre age est :<?php echo $_GET["age" ] ; ?> ans.
