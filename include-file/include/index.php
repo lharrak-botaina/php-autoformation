3883c4f4e0c88a8b16390cca22203f925d134d90
lharrak-botaina <lharrak.botaina@gmail.com>
2022-02-02 13:34:49 +0100

    add include exemp

---
+++ include-file/include/index.php
@@ -0,0 +1,15 @@
+<?php
+include 'function.php';
+?>
+
+    <section>
+        <div>
+            <h1>Hi there!</h1>
+        </div>
+    </section>
+    <?php
+    include 'function.php';
+    ?>
+
+</body>
+</html>
