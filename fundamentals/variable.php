8428ebce17e4e17b8c63f013c68ab9f83cb41012
lharrak-botaina <lharrak.botaina@gmail.com>
2022-02-04 11:23:38 +0100

    server exemple

--- fundamentals/variable.php
+++ fundamentals/variable.php
@@ -1,15 +1,15 @@
 <?php
 // A variable starts with the $ sign, followed by the name of the variable
 $txt = "Hello world!";
-$x = 5;
-$y = 10.5;
+// $x = 5;
+// $y = 10.5;
 
 echo $txt;
-echo "<br>";
-echo $x;
-echo "<br>";
-echo $y;
-echo "<br>";
+// echo "<br>";
+// echo $x;
+// echo "<br>";
+// echo $y;
+// echo "<br>";
 
-echo $x + $y;
+// echo $x + $y;
 ?>
