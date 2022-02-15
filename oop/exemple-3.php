51376bbf17bf690a921c48d1f06d959d76b693e2
lharrak-botaina <lharrak.botaina@gmail.com>
2022-02-15 12:44:48 +0100

    exemple 3

---
+++ oop/exemple-3.php
@@ -0,0 +1,37 @@
+<?php
+
+class BankAccount
+{
+	public $accountNumber;
+
+	public $balance;
+
+	public function deposit($amount)
+	{
+		if ($amount > 0) {
+			$this->balance += $amount;
+		}
+	}
+
+	public function withdraw($amount)
+	{
+		if ($amount <= $this->balance) {
+			$this->balance -= $amount;
+			return true;
+		}
+                return false;
+
+	}
+}
+
+    $account = new BankAccount;
+
+    $account->accountNumber = 1;
+    $account->balance = 100;
+
+    $respons =  $account->withdraw(30);
+
+    echo var_dump($respons);
+    echo'<br>';
+    echo 'rest in your account: '.  $account->balance;
+?>
