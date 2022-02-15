f15fe7b2da1c51c930b380236ad1f0da2dacbac6
lharrak-botaina <lharrak.botaina@gmail.com>
2022-02-15 11:40:41 +0100

    add exemple-1 oop

---
+++ recherche-array/index.php
@@ -0,0 +1,21 @@
+<?php
+$trouve = False;
+$listNumbers = array(1,4,64,65,33);
+
+// $listNumbers[0] = 1;
+// $listNumbers[1] = 10;
+// $listNumbers[2] = 4;
+// $listNumbers[3] = 987;
+// $listNumbers[4] = 9;
+$userInput = 11;
+for ($i = 0; $i <= 4; $i++) {
+    if ($userInput == $listNumbers[$i]) {
+        $trouve = True;
+    }
+}
+if ($trouve) {
+    echo "le nombre existe" ;
+} else {
+    echo "le nombre n'existe pas " ;
+}
+?>
